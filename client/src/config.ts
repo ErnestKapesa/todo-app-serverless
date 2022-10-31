98b71286555d110f5545493812520f6e7f28cf9b
ErnestKapesa <32816871+ErnestKapesa@users.noreply.github.com>
2022-10-31 03:12:40 +0200

    backend and client side set
    updates

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'ycybsoezpg';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-pu2cvl58b7l2mcgi.us.auth0.com',
+  clientId: 'zhfkQA0gQUsSLuyxVVInU3wtFifVjEAs',
+  callbackUrl: 'http://localhost:3000/callback'
+};
